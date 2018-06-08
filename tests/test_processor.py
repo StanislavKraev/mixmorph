42e949435aeff43e5169d8d8c15f6d0b68f701c8
Stanislav Kraev <stanislav.kraev@gmail.com>
2018-06-08 12:13:24 +0300

    event sequence

--- tests/test_processor.py
+++ tests/test_processor.py
@@ -15,3 +15,14 @@ def sc_processor_simple(sc_processor: SCProcessor, cases_path):
 def test_should_transit_from_a_to_b_when_event_alpha_triggered(sc_processor_simple):
     sc_processor_simple.on(Event(event_id="alpha"))
     assert State("b") == sc_processor_simple.state
+
+
+def test_should_not_transit_from_a_to_b_when_event_beta_triggered(sc_processor_simple):
+    sc_processor_simple.on(Event(event_id="beta"))
+    assert State("a") == sc_processor_simple.state
+
+
+def test_should_transit_from_a_to_c_when_events_alpha_beta_triggered(sc_processor_simple):
+    sc_processor_simple.on(Event(event_id="alpha"))
+    sc_processor_simple.on(Event(event_id="beta"))
+    assert State("c") == sc_processor_simple.state
